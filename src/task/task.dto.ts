4c8de26ad8876ac1fd4cd61096471e7e59a98c2a
LucasCidi <lucascidw@gmail.com>
2025-03-24 14:26:59 -0300

    integrando com db

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,33 @@
+import { IsDateString, IsEnum, IsOptional, IsString, isString, IsUUID, MaxLength, MinLength } from "class-validator";
+import { UUID } from "crypto";
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE'
+}
+
+
 export class TaskDto {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(100)
   title: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(300)
   description: string;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
   status: string;
+
+  @IsDateString()
   expirationDate: Date;
 }
 
